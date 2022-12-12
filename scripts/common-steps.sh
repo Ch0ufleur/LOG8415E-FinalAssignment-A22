166fde62e205a7ddbf90db87fdc34acf35f9e640
Ch0ufleur <jimkick3@outlook.com>
2022-12-11 19:35:02 -0500

    Update common step to add deploy dir

--- scripts/common-steps.sh
+++ scripts/common-steps.sh
@@ -4,6 +4,9 @@ cd /opt/mysqlcluster/home
 sudo wget http://dev.mysql.com/get/Downloads/MySQL-Cluster-7.2/mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz
 sudo tar xvf mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz
 sudo ln -s mysql-cluster-gpl-7.2.1-linux2.6-x86_64 mysqlc
-sudo echo 'sudo export MYSQLC_HOME=/opt/mysqlcluster/home/mysqlc' > /etc/profile.d/mysqlc.sh
-sudo echo 'sudo export PATH=$MYSQLC_HOME/bin:$PATH' >> /etc/profile.d/mysqlc.sh
-sudo source /etc/profile.d/mysqlc.sh
+sudo su
+echo 'export MYSQLC_HOME=/opt/mysqlcluster/home/mysqlc' > /etc/profile.d/mysqlc.sh
+echo 'export PATH=$MYSQLC_HOME/bin:$PATH' >> /etc/profile.d/mysqlc.sh
+exit
+source /etc/profile.d/mysqlc.sh
+sudo mkdir -p /opt/mysqlcluster/deploy/ndb_data
