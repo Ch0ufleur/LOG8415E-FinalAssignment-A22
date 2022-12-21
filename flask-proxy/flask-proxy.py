00ecc9b7f94d45e88e06482886ad753654d693de
Ch0ufleur <jimkick3@outlook.com>
2022-12-20 21:46:13 -0500

    References added to flask proxy

--- flask-proxy/flask-proxy.py
+++ flask-proxy/flask-proxy.py
@@ -13,6 +13,12 @@ def sql_query():
     return response
 
 def do_query(q:str):
+    server = SSHTunnelForwarder(
+        'alfa.8iq.dev',
+        ssh_username="pahaz",
+        ssh_password="secret",
+        remote_bind_address=('127.0.0.1', 8080)
+    )
     connection = pymysql.connect(host=selectHost(), user='finaltp', password='', database='sakila', charset='utf8mb4', bind_address=tunnel-ip)
     with connection:
         with connection.cursor() as cursor:
@@ -20,3 +26,21 @@ def do_query(q:str):
 
 def selectHost():
 
+"""
+from sshtunnel import open_tunnel
+from time import sleep
+
+with open_tunnel(
+    ('localhost', 2222),
+    ssh_username="vagrant",
+    ssh_password="vagrant",
+    remote_bind_address=('127.0.0.1', 3306)
+) as server:
+
+    print(server.local_bind_port)
+    while True:
+        # press Ctrl-C for stopping
+        sleep(1)
+
+print('FINISH!')https://pymysql.readthedocs.io/en/latest/user/examples.html
+""" https://pypi.org/project/sshtunnel/
